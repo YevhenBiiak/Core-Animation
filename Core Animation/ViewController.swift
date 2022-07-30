4be5442279b61a9cc77c0f00bfc6ccbe57cc57c9
macbook <yevhen.biiak@icloud.com>
2022-07-30 11:52:25 +0300

    initial commit

---
+++ Core Animation/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Core Animation
+//
+//  Created by Евгений Бияк on 30.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Core Animation' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Core Animation
+
+  pod 'SwiftLint'
+
+end
